66e763e0c1c10a31ce5a069fd6ef816e091cd68b
René de Hesselle <dehesselle@web.de>
2020-08-11 02:46:57 +0200

    Add string functions

---
+++ str_.sh
@@ -0,0 +1,34 @@
+include_guard
+
+### description ################################################################
+
+# This files provides convenience functions for working with strings.
+
+### includes ###################################################################
+
+# Nothing here.
+
+### variables ##################################################################
+
+# Nothing here.
+
+### functions ##################################################################
+
+function str_is_empty
+{
+  local string=$1
+
+  if [ ${#string} -eq 0 ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+### aliases ####################################################################
+
+# Nothing here.
+
+### main #######################################################################
+
+# Nothing here.
