f8d5bd2dbe9c73d1c37cebed4fcea979b8af6522
René de Hesselle <dehesselle@web.de>
2020-10-16 00:09:24 +0200

    Add switch to print ANSI in non-terminal mode

--- echo_.sh
+++ echo_.sh
@@ -9,26 +9,34 @@ include_file ANSI_.sh
 
 ### variables ##################################################################
 
-# Nothing here.
+ECHO_ANSI_ENABLE=true       # allow ANSI escape sequences for e.g. colors...
+ECHO_ANSI_TERM_ONLY=true    # ...but only when running in a terminal
 
 ### functions ##################################################################
 
 function _echo_message
 {
-  local name=$1    # optional
+  local name=$1   # can be empty
   local type=$2
   local color=$3
   local args=${@:4}
 
-  [ -z $name ] && name=$ANSI_CURSOR_LEFT
-  local color_reset=$ANSI_FG_RESET
+  if $ECHO_ANSI_ENABLE; then
+    local color_reset=$ANSI_FG_RESET
 
-  if [ ! -t 1 ]; then   # disable colors if not running in terminal
-    color=""
-    color_reset=""
+    # Disable colors if not running in terminal unless explicitly allowed
+    # by ECHO_ANSI_TERM_ONLY=false.
+    if [ ! -t 1 ] && $ECHO_ANSI_TERM_ONLY; then
+        color=""
+        color_reset=""
+    fi
   fi
 
-  echo -e "$color[$type] $name$color_reset $args"
+  if [ ! -z $name ]; then
+    name=":$name"
+  fi
+
+  echo -e "$color[$type$name] $color_reset$args"
 }
 
 ### aliases ####################################################################
