c1a6587a3c9ee4ea63979ef5c07aedaaee58b4cf
René de Hesselle <dehesselle@web.de>
2021-06-29 20:34:46 +0200

    Adopt shellcheck for xdg.sh

--- xdg.sh
+++ xdg.sh
@@ -1,7 +1,6 @@
+# SPDX-FileCopyrightText: 2021 René de Hesselle <dehesselle@web.de>
+#
 # SPDX-License-Identifier: GPL-2.0-or-later
-# https://github.com/dehesselle/bash_d
-
-include_guard
 
 ### description ################################################################
 
@@ -11,25 +10,29 @@ include_guard
 # You can either access the variables directly or use the helper-functions
 # 'xdg_get_xxx' that make sure the directories exist.
 
+### shellcheck #################################################################
+
+# shellcheck shell=bash # no shebang as this file is intended to be sourced
+
 ### includes ###################################################################
 
-# Nothing here.
+include_guard
 
 ### variables ##################################################################
 
-if [ -z $XDG_CONFIG_HOME ]; then
+if [ -z "$XDG_CONFIG_HOME" ]; then
   export XDG_CONFIG_HOME=$HOME/.config
 fi
 
-if [ -z $XDG_CACHE_HOME ]; then
+if [ -z "$XDG_CACHE_HOME" ]; then
   export XDG_CACHE_HOME=$HOME/.cache
 fi
 
-if [ -z $XDG_DATA_HOME ]; then
+if [ -z "$XDG_DATA_HOME" ]; then
   export XDG_DATA_HOME=$HOME/.local/share
 fi
 
-if [ -z $XDG_RUNTIME_DIR ]; then
+if [ -z "$XDG_RUNTIME_DIR" ]; then
   # This is a bad choice as it does not have properties that the spec requries
   # (user-owned, 700 permission, guaranteed creation/destruction on
   # login/logout), but I have nothing better to offer.
@@ -42,34 +45,35 @@ function xdg_get
 {
   local var=$1
 
-  eval local dir="\$$var"
+  local dir
+  eval dir="\$$var"
 
   # XDG directories are expected to be created if they don't exist.
   if [ ! -d "$dir" ]; then
     mkdir -p "$dir"
   fi
 
-  echo $dir
+  echo "$dir"
 }
 
 function xdg_get_config_home
 {
-  echo $(xdg_get XDG_CONFIG_HOME)
+  xdg_get XDG_CONFIG_HOME
 }
 
 function xdg_get_cache_home
 {
-  echo $(xdg_get XDG_CACHE_HOME)
+  xdg_get XDG_CACHE_HOME
 }
 
 function xdg_get_data_home
 {
-  echo $(xdg_get XDG_DATA_HOME)
+  xdg_get XDG_DATA_HOME
 }
 
 function xdg_get_runtime_dir
 {
-  echo $(xdg_get XDG_RUNTIME_DIR)
+  xdg_get XDG_RUNTIME_DIR
 }
 
 ### aliases ####################################################################
