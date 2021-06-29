d56b7a421183dd188ac36efcd2a7d51980e4b277
René de Hesselle <dehesselle@web.de>
2021-06-29 21:07:41 +0200

    Adopt shellcheck for daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -1,23 +1,28 @@
+# SPDX-FileCopyrightText: 2021 René de Hesselle <dehesselle@web.de>
+#
 # SPDX-License-Identifier: GPL-2.0-or-later
-# https://github.com/dehesselle/bash_d
-
-include_guard
 
 ### description ################################################################
 
-# This file provides functions to write simple daemons. You basically have
+# Provides functions to write simple daemons. You basically have
 # to override the function daemon_run with whatever you want to run and call
 # 'daemon_dispatch $1' as main function to get going.
 
+### shellcheck #################################################################
+
+# shellcheck shell=bash # no shebang as this file is intended to be sourced
+
 ### includes ###################################################################
 
-include_file echo.sh
-include_file xdg.sh
+include_guard
+
+include_file echo
+include_file xdg
 
 ### variables ##################################################################
 
 DAEMON_NAME=process
-DAEMON_PIDFILE=$XDG_RUNTIME_DIR/$(basename -- $0).pid
+DAEMON_PIDFILE=$XDG_RUNTIME_DIR/$(basename -- "$0").pid
 DAEMON_SHUTDOWN_GRACE=20
 
 ### functions ##################################################################
@@ -26,10 +31,10 @@ function daemon_dispatch
 {
   local command=$1
 
-  local pid_dir=$(dirname $DAEMON_PIDFILE)
-  if [ ! -d $pid_dir ]; then
-    mkdir -p $pid_dir
-    if [ $? -ne 0 ]; then
+  local pid_dir
+  pid_dir=$(dirname "$DAEMON_PIDFILE")
+  if [ ! -d "$pid_dir" ]; then
+    if mkdir -p "$pid_dir"; then
       echo_e "failed to create directory for pid: $pid_dir"
       return 1
     fi
@@ -37,12 +42,12 @@ function daemon_dispatch
 
   case "$command" in
     start)
-      local pid=0
-      if [ -f $DAEMON_PIDFILE ]; then
-         pid=$(cat $DAEMON_PIDFILE)
+      declare -i pid=0
+      if [ -f "$DAEMON_PIDFILE" ]; then
+         pid=$(cat "$DAEMON_PIDFILE")
       fi
-      if [ $pid -ne 0 ]; then
-         if [ $(ps -p $pid >/dev/null 2>&1; echo $?) -eq 0 ]; then
+      if [ "$pid" -ne 0 ]; then
+         if [ "$(ps -p "$pid" >/dev/null 2>&1; echo $?)" -eq 0 ]; then
             echo_e "$DAEMON_NAME running as pid $pid"
             return 1
          else
@@ -52,7 +57,7 @@ function daemon_dispatch
       daemon_start
       ;;
     restart|status|stop)
-      daemon_$command
+      daemon_"$command"
       ;;
     *)
       daemon_print_usage
@@ -69,8 +74,7 @@ function daemon_print_usage
 
 function daemon_restart
 {
-  daemon_stop
-  if [ $? -eq 0 ]; then
+  if daemon_stop; then
     daemon_start
   fi
 }
@@ -78,25 +82,25 @@ function daemon_restart
 function daemon_run
 {
   echo_e "You need to override this function."
-  echo $BASHPID > $DAEMON_PIDFILE   # you need to provide the pid
+  echo $BASHPID > "$DAEMON_PIDFILE"   # you need to provide the pid
 }
 
 function daemon_start
 {
-  if [ -f $DAEMON_PIDFILE ]; then
-    rm $DAEMON_PIDFILE
+  if [ -f "$DAEMON_PIDFILE" ]; then
+    rm "$DAEMON_PIDFILE"
   fi
 
   daemon_run &
 
-  local seconds=0
-  while [ ! -f $DAEMON_PIDFILE ] && [ $seconds -lt 10 ]; do
+  declare -i seconds=0
+  while [ ! -f "$DAEMON_PIDFILE" ] && [ $seconds -lt 10 ]; do
     ((seconds+=1))
     sleep 1
   done
 
-  if [ -f $DAEMON_PIDFILE ]; then
-    echo_o "$DAEMON_NAME started as pid $(cat $DAEMON_PIDFILE)"
+  if [ -f "$DAEMON_PIDFILE" ]; then
+    echo_o "$DAEMON_NAME started as pid $(cat "$DAEMON_PIDFILE")"
   else
     echo_e "no pidfile created"
   fi
@@ -104,10 +108,11 @@ function daemon_start
 
 function daemon_status
 {
-  if [ -f $DAEMON_PIDFILE ]; then
-    local pid=$(cat $DAEMON_PIDFILE)
+  if [ -f "$DAEMON_PIDFILE" ]; then
+    declare -i pid
+    pid=$(cat "$DAEMON_PIDFILE")
 
-    if [ $(ps -p $pid >/dev/null 2>&1; echo $?) -eq 0 ]; then
+    if [ "$(ps -p "$pid" >/dev/null 2>&1; echo $?)" -eq 0 ]; then
       echo_i "$DAEMON_NAME is alive (pid $pid)"
     else
       echo_w "$DAEMON_NAME has died (pid $pid)"
@@ -119,30 +124,31 @@ function daemon_status
 
 function daemon_stop
 {
-  if [ -f $DAEMON_PIDFILE ]; then
-    local pid=$(cat $DAEMON_PIDFILE)
+  if [ -f "$DAEMON_PIDFILE" ]; then
+    local pid
+    pid=$(cat "$DAEMON_PIDFILE")
 
-    if [ $(ps -p $pid >/dev/null 2>&1; echo $?) -eq 0 ]; then
-      kill -TERM $pid
+    if [ "$(ps -p "$pid" >/dev/null 2>&1; echo $?)" -eq 0 ]; then
+      kill -TERM "$pid"
 
-      local seconds=0
-      while [ $(ps -p $pid >/dev/null 2>&1; echo $?) -eq 0 ] && [ $seconds -lt $DAEMON_SHUTDOWN_GRACE ]; do
+      declare -i seconds=0
+      while [ "$(ps -p "$pid" >/dev/null 2>&1; echo $?)" -eq 0 ] && [ $seconds -lt $DAEMON_SHUTDOWN_GRACE ]; do
         echo_i "waiting for $DAEMON_NAME (pid $pid) to shut down..."
         sleep 5
         ((seconds+=5))
       done
 
-      if [ $seconds -gt $DAEMON_SHUTDOWN_GRACE ]; then
+      if [ "$seconds" -gt $DAEMON_SHUTDOWN_GRACE ]; then
         echo_e "$DAEMON_NAME (pid $pid) failed to shut down after $seconds seconds"
         return 1
       else
         echo_o "shut down"
-        rm $DAEMON_PIDFILE
+        rm "$DAEMON_PIDFILE"
         return 0
       fi
     else
       echo_w "$DAEMON_NAME (pid $pid) was already gone"
-      rm $DAEMON_PIDFILE
+      rm "$DAEMON_PIDFILE"
       return 0
     fi
   else
